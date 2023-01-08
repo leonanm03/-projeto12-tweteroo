51f2a271a4b49064922bceea709eb8b6537f64a4
Leonan <leonanm04@gmail.com>
2023-01-08 15:24:55 -0300

    server starting with door 5000 and cors added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("alou testando!!")
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.get("/hello", (req, res) => {
+  res.send("Meu primeiro servidor, yay!");
+});
+
+app.listen(5000, () => {
+  console.log("App server listening in door 5000");
+});
