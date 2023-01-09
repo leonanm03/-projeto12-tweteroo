bd8c2ddad4c36d3d813a11e31add53ace826ff56
Leonan <leonanm04@gmail.com>
2023-01-09 09:55:53 -0300

    User sign up working and blocking the sign of repeated users

--- src/app.js
+++ src/app.js
@@ -1,12 +1,43 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 
+const PORT = 5000;
 const app = express();
 app.use(cors());
-app.get("/hello", (req, res) => {
-  res.send("Meu primeiro servidor, yay!");
+app.use(json());
+
+const users = [
+  {
+    username: "teste",
+    avatar:
+      "https://image.disparada.com.br/wp-content/uploads/2021/03/11113736/img-bolsominion.jpg",
+  },
+];
+
+const tweets = [
+  {
+    username: "teste",
+    tweet: "batata!!",
+  },
+];
+
+app.post("/sign-up", (req, res) => {
+  const signUp = req.body;
+  const existingUser = users.some((u) => u.username === signUp.username);
+
+  if (existingUser) {
+    return res.status(409).send("Usuário já existe.");
+  }
+
+  if (signUp.username && signUp.avatar) {
+    users.push(signUp);
+    console.log(users);
+    return res.status(201).send("OK");
+  }
 });
 
-app.listen(5000, () => {
-  console.log("App server listening in door 5000");
+
+app.listen(PORT, () => {
+  console.log(`App server runing in http://localhost:${PORT}`);
+  console.log(users);
 });
